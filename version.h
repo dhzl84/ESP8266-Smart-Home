7679bfa56bf151cdff4770d55cfb5152cd0cc5c0
eXtatic <eXtatic@bossmail.de>
2018-05-12 13:37:59 +0200

    add release (r) and debug (d) prefixes to version string

--- version.h
+++ version.h
@@ -1,1 +1,9 @@
-#define FIRMWARE_VERSION "0.2.1"
+#define VERSION "0.2.1"
+
+#if defined CFG_DEBUG
+#define FW_PREFIX "d"
+#else
+#define FW_PREFIX "r"
+#endif
+
+#define FIRMWARE_VERSION (FW_PREFIX VERSION)
