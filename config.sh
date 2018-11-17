22a8258aba4c16c2250b9c73cf052b26e40efb1d
dhzl84 <mail@hauzel.net>
2018-11-18 00:00:14 +0100

    update config.sh for travis

--- config.sh
+++ config.sh
@@ -3,12 +3,14 @@ cat > config.h << EOF
 #define CONFIG_H_
 
 /* the config.h file contains your personal configuration of the parameters below: */
-#define WIFI_SSID          "xxx"
-#define WIFI_PWD           "xxx"
-#define LOCAL_MQTT_HOST    "123.456.789.012"
-#define LOCAL_MQTT_USER    "xxx"
-#define LOCAL_MQTT_PWD     "xxx"
-#define THERMOSTAT_BINARY  "http://<domain or ip>/<name>.bin"
+#define WIFI_SSID               "xxx"
+#define WIFI_PWD                "xxx"
+#define LOCAL_MQTT_HOST         "123.456.789.012"
+#define LOCAL_MQTT_PORT         1883
+#define LOCAL_MQTT_USER         "xxx"
+#define LOCAL_MQTT_PWD          "xxx"
+#define THERMOSTAT_BINARY       "http://<domain or ip>/<name>.bin"
+#define SENSOR_UPDATE_INTERVAL  20000
 
 #endif /* CONFIG_H_ */
 EOF
