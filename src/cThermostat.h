7f7ac45849de31c82d1701fe3125f5a0428aa19d
dhzl84 <mail@hauzel.net>
2018-12-27 10:37:03 +0100

    lint the code

--- src/cThermostat.h
+++ src/cThermostat.h
@@ -4,19 +4,19 @@
 #include "Arduino.h"
 
 #ifndef minTargetTemp
-  #define minTargetTemp (int)150  // 15.0 °C
+  #define minTargetTemp (int16_t)150  // 15.0 °C
 #endif
 
 #ifndef maxTargetTemp
-  #define maxTargetTemp (int)250  // 25.0 °C
+  #define maxTargetTemp (int16_t)250  // 25.0 °C
 #endif
 
 #ifndef maxHysteresis
-  #define maxHysteresis (int)20   // 2.0 °C
+  #define maxHysteresis (int16_t)20   // 2.0 °C
 #endif
 
 #ifndef minHysteresis
-  #define minHysteresis (int)2    // 0.2 °C
+  #define minHysteresis (int16_t)2    // 0.2 °C
 #endif
 
 #ifndef CFG_MEDIAN_QUEUE_SIZE
@@ -32,70 +32,70 @@ class Thermostat
     Thermostat(void);
     ~Thermostat(void);
     void init(void);
-    void setup(unsigned char gpio, unsigned char tarTemp, int calibFactor, int calibOffset, int tHyst);
+    void setup(uint8_t gpio, uint8_t tarTemp, int16_t calibFactor, int16_t calibOffset, int16_t tHyst);
     void loop(void);
     // heating
     void setActualState(bool value);
-    void setTargetTemperature(int value);
-    void increaseTargetTemperature(unsigned int value);
-    void decreaseTargetTemperature(unsigned int value);
+    void setTargetTemperature(int16_t value);
+    void increaseTargetTemperature(uint16_t value);
+    void decreaseTargetTemperature(uint16_t value);
     void resetTransmitRequest();
     void resetNewData();
     bool getActualState(void);
-    int  getTargetTemperature(void);
     bool getNewData();
     bool getThermostatMode();
     void setThermostatMode(bool value);
     void toggleThermostatMode();
+    int16_t getTargetTemperature(void);
 
     //sensor
-    void setCurrentTemperature(int temperature);
-    void setCurrentHumidity(int value);
+    void setCurrentTemperature(int16_t temperature);
+    void setCurrentHumidity(int16_t value);
     void setLastSensorReadFailed(bool value);
-    void setThermostatHysteresis(int hysteresis);
+    void setThermostatHysteresis(int16_t hysteresis);
     void resetNewCalib();
-    void setSensorCalibData(int factor, int offset, bool calib);
-    int  getSensorFailureCounter(void);
-    int  getCurrentTemperature(void);
-    int  getCurrentHumidity(void);
-    int  getFilteredTemperature(void);
-    int  getFilteredHumidity(void);
-    int  getSensorCalibOffset(void);
-    int  getSensorCalibFactor(void);
-    int  getThermostatHysteresis(void);
-    int  getThermostatHysteresisHigh(void);
-    int  getThermostatHysteresisLow(void);
+    void setSensorCalibData(int16_t factor, int16_t offset, bool calib);
     bool getSensorError(void);
     bool getNewCalib();
+    int16_t getSensorFailureCounter(void);
+    int16_t getCurrentTemperature(void);
+    int16_t getCurrentHumidity(void);
+    int16_t getFilteredTemperature(void);
+    int16_t getFilteredHumidity(void);
+    int16_t getSensorCalibOffset(void);
+    int16_t getSensorCalibFactor(void);
+    int16_t getThermostatHysteresis(void);
+    int16_t getThermostatHysteresisHigh(void);
+    int16_t getThermostatHysteresisLow(void);
 
   private:
     // heating
+    bool      newData;
     bool      thermostatMode;
     bool      actualState;
-    uint8     targetTemperature;
-    uint8     relayGpio;
-    bool      newData;
-    int       thermostatHysteresis;
-    int       thermostatHysteresisHigh;
-    int       thermostatHysteresisLow;
+    uint8_t   targetTemperature;
+    uint8_t   relayGpio;
+    int16_t   thermostatHysteresis;
+    int16_t   thermostatHysteresisHigh;
+    int16_t   thermostatHysteresisLow;
     // sensor
-    int       currentTemperature;
-    int       currentHumidity;
-    int       filteredTemperature;
-    int       filteredHumidity;
-    int       sensorFailureCounter;
-    int       tempOffset;
-    int       tempFactor;
     bool      sensorError;
     bool      newCalib;
-    const int sensorErrorThreshold = 3;
+    int16_t   currentTemperature;
+    int16_t   currentHumidity;
+    int16_t   filteredTemperature;
+    int16_t   filteredHumidity;
+    int16_t   sensorFailureCounter;
+    int16_t   tempOffset;
+    int16_t   tempFactor;
+    const int16_t sensorErrorThreshold = 3;
     // filter
     bool      tempValueQueueFilled;
-    int       tempValueSampleID;
-    int       tempValueQueue[CFG_MEDIAN_QUEUE_SIZE];
     bool      humidValueQueueFilled;
-    int       humidValueSampleID;
-    int       humidValueQueue[CFG_MEDIAN_QUEUE_SIZE];
+    int16_t   tempValueSampleID;
+    int16_t   tempValueQueue[CFG_MEDIAN_QUEUE_SIZE];
+    int16_t   humidValueSampleID;
+    int16_t   humidValueQueue[CFG_MEDIAN_QUEUE_SIZE];
 };
 
 #endif /* cTHERMOSTAT_H_ */
--- src/main.h
+++ src/main.h
@@ -1,28 +1,41 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
 #include "Arduino.h"
-#include "config.h"
 #include <FS.h> // SPIFFS
+#include "config.h"
+
+/* the config.h file contains your personal configuration of the parameters below: 
+  #define WIFI_SSID                   "xxx"
+  #define WIFI_PWD                    "xxx"
+  #define LOCAL_MQTT_USER             "xxx"
+  #define LOCAL_MQTT_PWD              "xxx"
+  #define LOCAL_MQTT_PORT             1234
+  #define LOCAL_MQTT_HOST             "123.456.789.012"
+  #define THERMOSTAT_BINARY           "http://<domain or ip>/<name>.bin"
+  #define SENSOR_UPDATE_INTERVAL      20      // seconds
+  #define THERMOSTAT_HYSTERESIS       2       // seconds 
+  #define WIFI_RECONNECT_TIME         30      // seconds
+  #define CFG_PUSH_BUTTONS            false
+*/
 
 /*===================================================================================================================*/
 /* variable declarations */
 /*===================================================================================================================*/
-struct configuration
-{
+struct configuration {
   char    name[64];
-  int     tTemp;  /* persistent target temperature */
-  int     tHyst;  /* thermostat hysteresis */
-  int     calibF;
-  int     calibO;
+  int16_t tTemp;  /* persistent target temperature */
+  int16_t tHyst;  /* thermostat hysteresis */
+  int16_t calibF;
+  int16_t calibO;
   char    ssid[64];
   char    wifiPwd[64];
   char    mqttHost[64];
-  int     mqttPort;
+  int16_t mqttPort;
   char    mqttUser[64];
   char    mqttPwd[64];
   char    updServer[256];
-  int     sensUpdInterval;
-  int     mqttPubCycleInterval;
+  int16_t sensUpdInterval;
+  int16_t mqttPubCycleInterval;
 };
 
 /*===================================================================================================================*/
@@ -62,14 +75,14 @@ String readSpiffs(String file);
 /*===================================================================================================================*/
 /* library functions */
 /*===================================================================================================================*/
-float  intToFloat(int intValue);
-int    floatToInt(float floatValue);
+float   intToFloat(int16_t intValue);
+int16_t floatToInt(float floatValue);
 String boolToStringOnOff(bool boolean);
 String boolToStringHeatOff(bool boolean);
-long TimeDifference(unsigned long prev, unsigned long next);
-long TimePassedSince(unsigned long timestamp);
-bool TimeReached(unsigned long timer);
-void SetNextTimeInterval(unsigned long& timer, const unsigned long step);
-bool splitSensorDataString(String sensorCalib, int *offset, int *factor);
+int32_t TimeDifference(uint32_t prev, uint32_t next);
+int32_t TimePassedSince(uint32_t timestamp);
+bool TimeReached(uint32_t timer);
+void SetNextTimeInterval(uint32_t& timer, const uint32_t step);
+bool splitSensorDataString(String sensorCalib, int16_t *offset, int16_t *factor);
 
 #endif /* _MAIN_H_ */
