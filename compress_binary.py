6219f065aa03b95ea12bd3f2ce2fbe2eb1b2bd4b
dhzl84 <mail@hauzel.net>
2022-03-27 03:21:48 +0200

    use f-string

--- compress_binary.py
+++ compress_binary.py
@@ -1,27 +1,27 @@
-""" compress the binary with gzip """
+"""compress the binary with gzip"""
 import gzip
 import shutil
 import os
 Import("env", "projenv")
 
 def compress_firmware(source, target, env):
-  """ function to compress the binary """
-  # """ Compress ESP8266 firmware using gzip for 'compressed OTA upload' """
-  # compressed binary_compressed shall replace the original binary_compressed
-  binary_compressed = env.subst("$BUILD_DIR") + os.sep + env.subst("$PROGNAME") + ".bin"
-  binary_uncompressed = binary_compressed + '.big'      # store original binary with other name
-  if os.path.exists(binary_compressed):
-      shutil.move(binary_compressed, binary_uncompressed)  # rename source file
-      print("Compressing firmware ...")
-      with open(binary_uncompressed, 'rb') as f_in:
-          with gzip.open(binary_compressed, 'wb') as f_out:
-              shutil.copyfileobj(f_in, f_out)
-      # """ Set modification time on compressed file so incremental build works """
-      shutil.copystat(binary_compressed, binary_uncompressed)
+    """function to compress the binary"""
+    # """ Compress ESP8266 firmware using gzip for 'compressed OTA upload' """
+    # compressed binary_compressed shall replace the original binary_compressed
+    binary_compressed = env.subst("$BUILD_DIR") + os.sep + env.subst("$PROGNAME") + ".bin"
+    binary_uncompressed = binary_compressed + '.big'      # store original binary with other name
+    if os.path.exists(binary_compressed):
+        shutil.move(binary_compressed, binary_uncompressed)  # rename source file
+        with open(binary_uncompressed, 'rb') as f_in:
+            with gzip.open(binary_compressed, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+        # """ Set modification time on compressed file so incremental build works """
+        shutil.copystat(binary_compressed, binary_uncompressed)
 
-  if os.path.exists(binary_compressed):
-      org_firmware_size = os.stat(binary_uncompressed).st_size
-      gz_firmware_size = os.stat(binary_compressed).st_size
-      print("Compression reduced firmware size to {:.0f}% (was {} bytes, now {} bytes)".format((gz_firmware_size / org_firmware_size) * 100, org_firmware_size, gz_firmware_size))
+    if os.path.exists(binary_compressed):
+        org_firmware_size = os.stat(binary_uncompressed).st_size
+        gz_firmware_size = os.stat(binary_compressed).st_size
+        compression = (gz_firmware_size / org_firmware_size) * 100
+        print(f"Compression reduced firmware size to {compression:0.1f} % (was {org_firmware_size} bytes, now {gz_firmware_size} bytes)")
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", compress_firmware)
